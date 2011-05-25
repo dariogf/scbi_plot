3db42d061904a5a0ace4acedd4dd7c075575fbad
Dario Guerrero <dariogf@gmail.com>
2011-05-25 15:01:18 +0200

    added line plot

---
+++ lib/scbi_plot/histogram.rb
@@ -0,0 +1,126 @@
+require 'gnuplot'
+
+# = scbi_plot - Ploting utilities
+#
+# Plots a graph using gnuplot.
+#
+# =Usage:
+# p=ScbiPlot::Histogram.new('few_strings.png','titulo')
+#
+# p.add_x(['a','b','c'])
+# p.add_y([10,20,30])
+#
+# p.do_graph
+#
+# =Tips
+# * scbi_plot can handle integer and string values in x row
+# * If x and y are integer values, the graph is sorted by x (ascendent values from left to right)
+# * When x contains strings:
+#     -if number of elements in x is greater than x_limit (by default 20), values are sorted in descendant order, and only @x_limit values are plotted
+#     -if number of elements is below x_limit, then values are shown as an histogram
+module ScbiPlot
+
+  class Histogram < ScbiPlot::Plot
+
+
+    def initialize(file_name,title=nil)
+      super
+      @line_width=4
+    end
+
+    def do_graph
+      setup_data
+      $VERBOSE=false
+
+      Gnuplot.open do |gp|
+        # histogram
+        Gnuplot::Plot.new( gp ) do |plot|
+
+          if !title
+            title=file_name
+          end
+
+          plot.title "#{@title}"
+          plot.xlabel @x_label
+          plot.ylabel @y_label
+
+          if !@show_leyend
+            plot.set "key off" #leyend
+          end
+
+          # x values are integers
+          if !contains_strings?(@x)
+            plot.style "fill  pattern 22  border -1"
+            plot.set "boxwidth 0.2" # Probably 3-5.
+
+            plot.data << Gnuplot::DataSet.new( [@x, @y] ) do |ds|
+              ds.with=  " imp lw #{@line_width}"
+            end
+
+          else #graph with strings in X axis
+            plot.xlabel ""
+
+            plot.set "style fill solid 1.00 border -1"
+            plot.set "style histogram clustered gap 1 title offset character 0, 0, 0"
+            plot.set "style data histogram"
+            plot.set "boxwidth 0.2 absolute"
+
+            if @x.count>4 then
+              plot.set "xtics offset 0,graph 0 rotate 90"
+            end
+
+            plot.data << Gnuplot::DataSet.new( [@x,@y] ) do |ds|
+              ds.using = "2:xticlabels(1)"   #show the graph and use labels at x
+            end
+
+          end
+
+          if !file_name.nil?
+            plot.terminal "png size 800,600"
+            plot.output "#{@file_name}"
+          end
+        end
+      end
+    end
+
+
+
+    def setup_data
+      if @x.length != @y.length
+        raise "Variables x and y have different sizes"
+
+      else
+
+        hash=xy_to_hash(@x,@y)
+        # puts hash
+        # sort integer data
+        if !contains_strings?(@x)
+
+          @x.sort!
+          @y=[]
+
+          @x.each do |v|
+            @y.push hash[v].to_i
+          end
+
+
+        else # there are strings in X
+          @x=[]
+          @y=[]
+
+          # save quoted values
+          hash.keys.each do |v|
+            @x.push "\"#{v.gsub('\"','').gsub('\'','')}\""
+            @y.push hash[v.to_s]
+          end
+
+          # check if there is a lot of string data
+          check_data_limit
+
+        end
+      end
+    end
+
+
+  end
+end
---
+++ lib/scbi_plot/lines.rb
@@ -0,0 +1,114 @@
+require 'gnuplot'
+
+# = scbi_plot - Ploting utilities
+#
+# Plots a graph using gnuplot.
+#
+# =Usage:
+# p=ScbiPlot::Histogram.new('few_strings.png','titulo')
+#
+# p.add_x(['a','b','c'])
+# p.add_y([10,20,30])
+#
+# p.do_graph
+#
+# =Tips
+# * scbi_plot can handle integer and string values in x row
+# * If x and y are integer values, the graph is sorted by x (ascendent values from left to right)
+# * When x contains strings:
+#     -if number of elements in x is greater than x_limit (by default 20), values are sorted in descendant order, and only @x_limit values are plotted
+#     -if number of elements is below x_limit, then values are shown as an histogram
+module ScbiPlot
+
+  class Lines < ScbiPlot::Plot
+
+
+    def initialize(file_name,title=nil)
+      super
+      @series=[]
+      @line_width=2
+      @show_leyend=true
+      @vertical_lines=[]
+
+
+
+    end
+
+    def add_series(title, y_data, graph_type ='lines',line_width=nil)
+      @series << {:title => title, :data=> y_data, :graph_type=>graph_type, :line_width=>(line_width || @line_width)}
+    end
+
+    def add_vertical_line(title,x_value)
+      @vertical_lines << {:title => title, :x_value=> x_value}
+    end
+
+    def do_graph
+      setup_data
+      $VERBOSE=true
+
+      Gnuplot.open do |gp|
+        # histogram
+        Gnuplot::Plot.new( gp ) do |plot|
+
+          if !title
+            title=file_name
+          end
+
+          plot.title "#{@title}"
+          plot.xlabel @x_label
+          plot.ylabel @y_label
+
+          if !@show_leyend
+            plot.set "key off" #leyend
+          end
+
+          # x values are integers
+          if contains_strings?(@x)
+            raise "Line plots cannot have strings in x axis"
+          end
+
+          # plot.style "fill pattern 22 border -1"
+          # plot.set "boxwidth 0.2" # Probably 3-5.
+          if !@vertical_lines.empty?
+            x2ticks =[]
+            @vertical_lines.each do |v_line|
+
+              x2ticks << "'#{v_line[:title]} (#{v_line[:x_value]})' #{v_line[:x_value]}"
+            end
+
+            plot.set "x2tics (#{x2ticks.join(' ')})"
+            plot.set 'grid noxtics x2tics lt rgb "green"'
+          end
+
+          @series.each do |series|
+            plot.data << Gnuplot::DataSet.new( [@x, series[:data]] ) do |ds|
+
+              ds.with =  series[:graph_type]
+              ds.linewidth = series[:line_width]
+              ds.title = series[:title]
+
+            end
+
+          end
+
+          if !file_name.nil?
+            plot.terminal "png size 800,600"
+            plot.output "#{@file_name}"
+          end
+        end
+      end
+    end
+
+
+
+    def setup_data
+      @series.each do |series|
+        if @x.length != series[:data].length
+          raise "Variables x and y have different sizes in serie #{series[:name]}"
+        end
+      end
+    end
+
+
+  end
+end
